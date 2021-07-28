981946049c26b1c5886c939a030ff567456fdaa8
ljbrewer <ljbrewer.lb@gmail.com>
2021-07-28 12:57:49 -0700

    update with working colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,46 +4,71 @@ var today = moment();
 $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 
 //creat the array of the hours
-let hour=document.querySelectorAll("div.hour")
+let hour=document.querySelectorAll("div.hour");
 
 //Loop thru the items in the array to color them
 for (let index = 0; index < hour.length; index++) {
-    var element = hour[index]
+    var element = hour[index];
     //Get the sibling to the chosen element
-    sibling=element.nextElementSibling
+    sibling=element.nextElementSibling;
   
-    //set the time to specific hour
-    let time=element.dataset.hour
-
     // if the current hour is past then add the class to make it past
     if (element.dataset.hour < moment().hour()) {
 
-      element.classList.add("past")
+      element.classList.add("past");
       //color the siblint the same color
-      sibling.classList.add("past")
+      sibling.classList.add("past");
       
     } else //the hour equals the current hour color it for Present time
         if (element.dataset.hour  == moment().hour()){
 
-           element.classList.add("present")
+           element.classList.add("present");
             //color the siblint the same color
-           sibling.classList.add("present")
+           sibling.classList.add("present");
 
     } else{
         //if the time is not past or present it must be future
-            element.classList.add("future")
+            element.classList.add("future");
              //color the siblint the same color
-            sibling.classList.add("future")
+            sibling.classList.add("future");
 
     }
-    console.log(element.classList.value)
-    console.log(element.dataset.hour + " data-hour")
 }
 
 
 
+// $(".saveBtn").on('click', function(){
 
-    
+//     var currentTime = {
+//         description: $(this).siblings(".description").val,
+//         reminder: $(this).siblings(".description").val,
+//         time: $(this).siblings(".time").text
+//     };
+
+
+
+
+// localStorage.setItem("workHours",currentTime );
+
+//   //  JSON.stringify(currentTime)//
+
+// }
+
+
+// //localStorage.setItem("workHours", JSON.stringify(workHours));
+
+
+// //var lastHours =JSON.parse(localStorage.getItem("workHours"));
+
+
+// // if (lastHours !== null){
+// //     document.hour = lastHours.time;
+// //     document.description = lastHours.description;
+// //     document.reminder = lastHours.reminder;
+
+// //}
+
+// )  
 
 
 

6ce0817735bc003dbe9eefc2b0344b1ac4201320
ljbrewer <ljbrewer.lb@gmail.com>
2021-07-27 17:16:57 -0700

    uploading new code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,80 +3,42 @@ var today = moment();
 //Displays current day at the top of the page
 $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 
-var workHours= [
-    {
-        id: "1",
-        hour: "9",
-        description: "",
-        reminder: ""
-    } ,  
-      {
-        id: "2",
-        hour: "10",
-        description: "",
-        reminder: ""
-    },
-    {
-        id: "3",
-        hour: "11",
-        description: "",
-        reminder: ""
-    },
+let hour=document.querySelectorAll("div.hour")
+//console.log(hour);
+    
+for (let index = 0; index < hour.length; index++) {
+    var element = hour[index]
+    console.log(element +" element")
+    let time=element.dataset.hour
+ //   console.log(time +" time")
+    
+ //   var time = parseInt(element.dataset.hour);
+    console.log(element.classList.value)
+  
+ console.log(moment().hour()+" moment hour")
 
-    {
-        id: "4",
-        hour: "12",
-        description: "",
-        reminder: ""
-    },
-    {
-        id: "5",
-        hour: "1",
-        description: "",
-        reminder: ""
-    },
-    {
-        id: "6",
-        hour: "2",
-        description: "",
-        reminder: ""
-    },
-    {
-        id: "7",
-        hour: "3",
-        description: "",
-        reminder: ""
-    },
-    {
-        id: "8",
-        hour: "4",
-        description: "",
-        reminder: ""
-    }, 
-    {
-        id: "9",
-        hour: "5",
-        description: "",
-        reminder: ""
-    },
-];
+    if (element.dataset.hour < moment().hour()) {
 
-if (document.getElementsByClassName("hour") < moment().format("HH A")) {
-    $("div:hour").addClass("past")
-} else 
-    if (document.getElementsByClassName("hour") = moment().format("HH A")){
-        $("div:hour").addClass("present")
-    } else
-        $("div:hour").addClass("future")
+      element.classList.add("past")
+      
+    } else 
+        if (element.dataset.hour  === moment().hour()){
+
+           element.classList.add("present")
+        
+
+    } else{
+       
+            element.classList.add("future")
+         
+
+    }
+    console.log(element.classList.value)
+    console.log(element.dataset.hour + " data-hour")
+}
 
 
 
-
-// saves data to localStorage
-//function saveHoursData(hour) {
-   
-      // save some stuff  
-//}
 
     
 
--- assets/old/script.js
+++ assets/old/script.js
@@ -3,72 +3,7 @@ var today = moment();
 //Displays current day at the top of the page
 $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 
-var workHours = [
-    {
-        id: "0",
-        hour: "9",
-        time: "9",
-        meridiem: "am",
-        reminder: ""
-    },
-    {
-        id: "1",
-        hour: "10",
-        time: "10",
-        meridiem: "am",
-        reminder: ""
-    },
-    {
-        id: "2",
-        hour: "11",
-        time: "11",
-        meridiem: "am",
-        reminder: ""
-    },
-    {
-        id: "3",
-        hour: "12",
-        time: "12",
-        meridiem: "pm",
-        reminder: ""
-    },
-    {
-        id: "4",
-        hour: "1",
-        time: "13",
-        meridiem: "pm",
-        reminder: ""
-    },
-    {
-        id: "5",
-        hour: "2",
-        time: "14",
-        meridiem: "pm",
-        reminder: ""
-    },
-    {
-        id: "6",
-        hour: "3",
-        time: "15",
-        meridiem: "pm",
-        reminder: ""
-    },
-    {
-        id: "7",
-        hour: "4",
-        time: "16",
-        meridiem: "pm",
-        reminder: ""
-    },
-    {
-        id: "8",
-        hour: "5",
-        time: "17",
-        meridiem: "pm",
-        reminder: ""
-    },
 
-]
 // saves data to localStorage
 function saveReminders() {
     localStorage.setItem("workHours", JSON.stringify(workHours));

6372154dc30827495a0c3b351df58e9ef786535d
ljbrewer <ljbrewer.lb@gmail.com>
2021-07-27 17:47:46 -0700

    uploading working code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,34 +3,37 @@ var today = moment();
 //Displays current day at the top of the page
 $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 
+//creat the array of the hours
 let hour=document.querySelectorAll("div.hour")
-//console.log(hour);
-    
+
+//Loop thru the items in the array to color them
 for (let index = 0; index < hour.length; index++) {
     var element = hour[index]
-    console.log(element +" element")
-    let time=element.dataset.hour
- //   console.log(time +" time")
-    
- //   var time = parseInt(element.dataset.hour);
-    console.log(element.classList.value)
+    //Get the sibling to the chosen element
+    sibling=element.nextElementSibling
   
- console.log(moment().hour()+" moment hour")
+    //set the time to specific hour
+    let time=element.dataset.hour
 
+    // if the current hour is past then add the class to make it past
     if (element.dataset.hour < moment().hour()) {
 
       element.classList.add("past")
+      //color the siblint the same color
+      sibling.classList.add("past")
       
-    } else 
-        if (element.dataset.hour  === moment().hour()){
+    } else //the hour equals the current hour color it for Present time
+        if (element.dataset.hour  == moment().hour()){
 
            element.classList.add("present")
-        
+            //color the siblint the same color
+           sibling.classList.add("present")
 
     } else{
-       
+        //if the time is not past or present it must be future
             element.classList.add("future")
-         
+             //color the siblint the same color
+            sibling.classList.add("future")
 
     }
     console.log(element.classList.value)
